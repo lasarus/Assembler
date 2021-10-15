e3de0197ac692ecfade87a29b49a4d4e1ced1f8b
Lucas Unnerfelt <llasarus@gmail.com>
2021-10-15 16:17:11 +0200

    Split instruction encoding into seperate source files.

---
+++ src/encoder.h
@@ -0,0 +1,12 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+// The encoder encodes instructions into machine code
+
+#include "parser.h"
+
+#include <stdint.h>
+
+void assemble_instruction(uint8_t *output, int *len, const char *mnemonic, struct operand ops[4]);
+
+#endif
